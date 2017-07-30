71b53d1d1822f0f2486f31586d258afaca3d0894
SATOIMO4175 <21411175ms@tama.ac.jp>
2017-07-31 00:34:05 +0900

    recommit

--- database.php
+++ database.php
@@ -1,8 +1,6 @@
 <?php
   // 重要：
   // このファイルのファイル名を database.php に変更すること。
-  // このままのファイル名で commit/push すると、github で公開されてしまう。
-  // username, password を、それぞれ自分のデータベースユーザ名・パスワードに変更する。
-  $db = mysqli_connect("iss.edu.tama.ac.jp", "idehara", "test", "idehara");
-  mysqli_set_charset($db, 'utf8');
+  // username, password を、それぞれ自分のデータベースユーザ名・パスワードに変更すること。
+  $db = mysqli_connect("1234567890@tama.ac.jp", "username", "password", "AAAA");
  ?>
--- newuser.php
+++ newuser.php
@@ -7,6 +7,7 @@
   </head>
   <body>
 
+#ここはいじらないでおこう。ダブリ防止は必要だからね。
 <?php
   include_once('database.php');
   if( isset($_POST['uid']) && strlen($_POST['uid'])>0 )
@@ -17,13 +18,14 @@
     if( $q->errno != 0 )
       print("登録失敗。ID が既に取得されています");
     else
-    {
+    {    
       print("登録しました");
       $_SESSION['login'] = $_POST['uid'];
       $_SESSION['name'] = $_POST['name'];
     }
   }
   else
+  
   {
     print "
     <form action='' method='POST'>
@@ -35,6 +37,34 @@
     ";
   }
  ?>
+
+<?php
+
+ include_once('database.php');
+ #if( isset($_POST['uid']) && if(strpos($_POST['uid'],'tama.ac.jp') === false){a}
+ if(!preg_match('tama.ac.jp',$_POST['uid'])){
+  print("登録失敗。無効なメールアドレスです");
+  
+  #tama.ac.jpが無いと出来ないようにしようとした。しかし、発動しない。
+  #原因は読んでも全く解決方法が浮かばない！
+  #英文字が使えないと書いてある気がするけど？じゃあどうすればいいの？
+  
+  #else
+   #このelseを有効にするとページの表示すら出来ない。
+   {    
+      print("登録しました");
+      $_SESSION['login'] = $_POST['uid'];
+      $_SESSION['name'] = $_POST['name'];
+    }
+  
+}
+ 
+?>
+
+#どんなにやってもエラーしか吐かない。
+#いったい何がどうしてどうやったらどうなるのだろう？
+#調べるにも何を調べれば？出てきたものを付け加えてもエラーしか吐かない。
+#いくらいじっても何もわからなかった。
 
   </body>
 </html>
