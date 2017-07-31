11b1d9e262ab331777a39635ffe6334f8bacfdc6
東野里芋 <21411175ms@tama.ac.jp>
2017-07-31 22:42:11 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,22 @@
+﻿<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>New User</title>
+  </head>
+  <body>
+  
+  <?php
+  
+  $a = 5;
+  $b = 8;
+  if($a > $b);
+  echo "A!!"
+  
+  ?>
+  </body>
+  
+  <!--あれ？bが大きいのにA!!が表示されるよ？-->
+  
+</html>
